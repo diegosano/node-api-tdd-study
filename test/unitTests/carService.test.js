4f026e5eb0679f74eaa15ebb70953e8572dc50ea
Diego Sano <diego.sano@nave.rs>
2022-10-10 11:40:20 -0300

    test: carService calculateFinalPrice method

--- test/unitTests/carService.test.js
+++ test/unitTests/carService.test.js
@@ -71,4 +71,28 @@ describe('CarService Suite Tests', () => {
     expect(carService.carRepository.find.calledWithExactly(car.id)).to.be.ok;
     expect(result).to.be.deep.equal(expected);
   });
+
+  it('should calculate final amount in real given a carCategory, customer and numberOfDays', () => {
+    const customer = Object.create(mocks.validCustomer);
+    customer.age = 50;
+
+    const carCategory = Object.create(mocks.validCarCategory);
+    carCategory.price = 37.6;
+
+    const numberOfDays = 5;
+
+    sandbox.stub(
+      carService,
+      'taxesBasedOnAge'
+    ).get(() => [{ from: 40, to: 50, then: 1.3 }]);
+
+    const expected = carService.currencyFormat.format(244.40);
+    const result = carService.calculateFinalPrice(
+      customer,
+      carCategory,
+      numberOfDays
+    );
+
+    expect(result).to.be.deep.equal(expected);
+  })
 });
