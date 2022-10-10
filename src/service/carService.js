0ec6add5cad41396a8e620661eb112b91772d2e4
Diego Sano <diego.sano@nave.rs>
2022-10-10 11:40:40 -0300

    feat: carService calculateFinalPrice method

--- src/service/carService.js
+++ src/service/carService.js
@@ -1,8 +1,16 @@
+const Tax = require('../entities/tax');
 const BaseRepository = require('../repository/base/baseRepository');
 
 class CarService {
   constructor({ cars }) {
     this.carRepository = new BaseRepository({ file: cars });
+
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge;
+
+    this.currencyFormat = Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
   }
 
   getRandomPositionFromArray(list) {
@@ -18,6 +26,17 @@ class CarService {
     const carId = this.chooseRandomCar(carCategory);
     const car = await this.carRepository.find(carId);
     return car;
+  }
+
+  calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer;
+    const { price } = carCategory;
+    const { then: tax } = this.taxesBasedOnAge.find(tax => age >= tax.from && age <= tax.to);
+
+    const finalPrice = ((tax * price) * numberOfDays);
+    const formattedFinalPrice = this.currencyFormat.format(finalPrice);
+
+    return formattedFinalPrice;
   }
 }
 
